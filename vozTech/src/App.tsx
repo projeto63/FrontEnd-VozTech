e0693f2e8a86bc0945c1cc1a72da713d1874bc7e
Gustavoleal1 <goialeal20@gmail.com>
2023-05-22 14:42:09 -0300

    atualização de login

--- vozTech/src/App.tsx
+++ vozTech/src/App.tsx
@@ -6,8 +6,9 @@ import { Grid } from "@material-ui/core";
 import Home from './assets/paginas/home/Home';
 import './App.css'
 import Login from './assets/paginas/login/Login';
-
-
+import Cadastro from './assets/paginas/cadastro/Cadastro';
+// import ListaTema from './assets/componentes/estaticos/temas/listatemas/ListaTema';
+// import ListaPostagem from './assets/componentes/postagens/listapostagens/ListaPostagem';
 
 function App() {
   return (
@@ -20,7 +21,9 @@ function App() {
           <Route path="/" element={<Login />}/>
           <Route path="/login" element={<Login />}/>
           <Route path="/home" element={<Home />}/>
-
+          <Route path="/cadastro" element={<Cadastro />}/>
+          {/* <Route path="/temas" element={<ListaTema />}/>
+          <Route path="/posts" element={<ListaPostagem />}/> */}
         </Routes>
         </div>
       <Footer />
--- vozTech/src/assets/paginas/login/Login.tsx
+++ vozTech/src/assets/paginas/login/Login.tsx
@@ -1,37 +1,78 @@
-import React from "react";
+import React, {ChangeEvent, useState, useEffect} from "react";
 import "./Login.css";
 import Logo from "../../imagens/voztech1.png" 
 import { Grid, Box, Typography, TextField, Button } from "@mui/material";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import UserLogin from '../../../models/UserLogin'
+import useLocalStorage from 'react-use-localstorage'
+import { login } from '../../../services/Services'
+
+function Login(){
+    const navigate = useNavigate();
+    const [token, setToken] = useLocalStorage('token'); //faz o controle do token dentro do localStorage
+    const[userLogin, setUserLogin] = useState<UserLogin>({
+  
+      id: 0,          //valores zerados, pois não foi feito nenhum cadastro/login
+      usuario: '',
+      senha: '',
+      token: '' 
+    }
+      )
+  
+      function updatedModel(e: ChangeEvent<HTMLInputElement>){
+  
+        setUserLogin({
+          ...userLogin,
+          //[e.target.name] : se referindo ao nome do campo, no caso onde será feia a mudança -> captura a propriedade
+          // e.target.value : é o valor que é alterado de fato no momento que o usuário digitar algo -> captura o valor digitado
+          [e.target.name]: e.target.value    //é utilizada para atualizar a model com o valor que o usuário digitar no campo de input | é um objeto com chave e valor [] representa o nome do campo e value -> o valor que o usuário digitou
+        })
+  
+      }
+  
+      useEffect(()=>{                 
+          if (token != ''){           
+            navigate('/home')
+          }                
+      }, [token])                           
+  
+      async function onSubmit(e: ChangeEvent<HTMLFormElement>){
+          e.preventDefault(); //impede que o botão atualize a tela
+          try{
+              await login(`/usuarios/logar`, userLogin, setToken)           //resposta da requisição da API
+              // setToken(resposta.data.token) //responsável por gravar o token que vem da API no localStorage
+  
+              alert('Usuário logado com sucesso!');
+          }catch(error){
+              alert('Dados do usuário inconsistentes. Erro ao logar!')
+          }
+          // console.log('userLogin ' + Object.values(userLogin))                //verifica se os dados inseridos em login estão corretos - foi feito só para teste 
+      }
 
 
 
-function Login () {
     return (
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center" className="imagem">
                 <Grid alignItems="center" item xs={8}>
                     <Box paddingX={20} className="formulario" justifyContent="center">
-                        <form>
-                            <Box className="logo">
-                            <img src= {Logo} alt="logo" />
-                            </Box>
-                            <Typography variant="h3" gutterBottom component="h3" align="center" className="textos1">Entrar</Typography>
-                            <TextField id="usuario" label="Usuário" variant="outlined" name="usuario" margin="normal" fullWidth />
-                            <TextField id="senha" label="Senha" variant="outlined" name="senha" margin="normal" type="password" fullWidth />
+                    <form onSubmit={onSubmit}>
+                            <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className="textos1">Entrar</Typography>
+                            <TextField value={userLogin.usuario} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="usuario" label="Usuário" variant="outlined" name="usuario" margin="normal" fullWidth />
+                            <TextField value={userLogin.senha} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="senha" label="Senha" variant="outlined" name="senha" margin="normal" type="password" fullWidth />
                             <Box marginTop={2} textAlign="center">
-                                <Link to="/home" className="text-decorator-none">
-                                <Button type="submit" variant="contained" className="botaologin">
-                                    Login
+                                <Button type="submit" variant="contained" color="primary">
+                                    Logar
                                 </Button>
-                                </Link>
                             </Box>
                         </form>
                         <Box display="flex" justifyContent="center" marginTop={2}>
                             <Box marginRight={1}> 
                                 <Typography variant="subtitle1" gutterBottom align="center">Não tem uma conta?</Typography>
                             </Box>
+                            <Link to = "/cadastro">
                                 <Typography variant="subtitle1" gutterBottom align="center" className="textos1">Cadastre-se </Typography>
+                        </Link>
                         </Box>
                     </Box>
                 </Grid>
@@ -41,4 +82,4 @@ function Login () {
     )
 }
 
-export default Login
+export default Login 
