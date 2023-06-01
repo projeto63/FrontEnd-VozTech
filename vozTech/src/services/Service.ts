2afd95f57d7e3593388863b03e0591661cec8092
BiancaMelquiades <biancamelquiadesbia@gmail.com>
2023-06-01 11:50:00 -0300

    processo de deploy

--- vozTech/src/services/Service.ts
+++ vozTech/src/services/Service.ts
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: "https://voztech.onrender.com/"
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 export const login = async (url: any, dados: any, setDados: any) => {
