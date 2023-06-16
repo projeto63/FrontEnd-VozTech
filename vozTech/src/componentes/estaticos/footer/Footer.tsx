755064d40a2ad150e1dd7e2dc6ba5b7882678ae5
Adriane Felicio <adrianefelicios@outlook.com>
2023-06-16 13:05:00 -0300

    footer

--- vozTech/src/componentes/estaticos/footer/Footer.tsx
+++ vozTech/src/componentes/estaticos/footer/Footer.tsx
@@ -16,26 +16,35 @@ function Footer() {
 
     if (token !== ""){
         footerComponent = 
-        <Grid container direction="row" justifyContent="center" alignItems="center">
-        <Grid  item xs={12} className="footer">
- 
-        <Box display={'flex'}  gap={2}  className='iconesFooter' alignItems={'center'}  justifyContent={'space-around'}>
-        <Box className="nomefooter">
-        <Typography className="itemsfooter" variant='h6'>Projeto VozTech</Typography>
-        <Typography className="itemsfooter" variant='h6'>Em parceria com Generation Brasil</Typography>
-        </Box>
-        <Box mx={10} justifyContent='center' display='flex' alignItems='center' className="icones">
-            <a href="https://github.com/projeto63/FrontEnd-VozTech" target='_blank'>
-            <GitHubIcon className='iconeInd1' />
-            </a>
-            <a href="https://linktr.ee/projetovoztech"target='_blank' >
-            <LinkedInIcon className='iconeInd2' />
-            </a>
+        <Box display={'flex'} className='footer'>
+            <Grid container direction="row" justifyContent='flex-end' alignItems="center">
+                <Grid alignItems="center" justifyContent='center' container xs={4}>
+                    <Box>
+                        <img src="/src/assets/logo-bege.svg" alt="" />
+                    </Box>
+                </Grid>
+                <Grid alignItems="center" direction='column' justifyContent='center' container xs={4}>
+                    <Box marginBottom={2} display='flex' gap={2}>
+                        <a href="https://github.com/projeto63">
+                        <GitHubIcon className='iconeInd1' />
+                        </a>
+                        <a href="https://linktr.ee/voztech">
+                        <LinkedInIcon className='iconeInd2' />
+                        </a>
+                    </Box>
+                        <Typography className="frasefooter">Projeto VozTech</Typography>
+                        <Typography className="frasefooter">Em parceria com Generation Brasil</Typography>
+                </Grid>
+                <Grid alignItems="center"  justifyContent='center' container xs={4}>
+                    <Box display='flex' gap={1}>
+						{/* <Link className='reset-link' to='/loja'>
+							{/* <Box mx={1} style={{ cursor: 'pointer' }}>
+							</Box> */}
+						{/* </Link> */}
+                    </Box>
+                </Grid>
+            </Grid>
             </Box>
-
-        </Box>
-        </Grid>
-        </Grid>
     
     }
     return (
