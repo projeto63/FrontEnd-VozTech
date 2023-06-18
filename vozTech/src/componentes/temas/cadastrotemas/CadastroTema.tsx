56657ed59135f10b718711741c299947f485d9d6
BiancaMelquiades <biancamelquiadesbia@gmail.com>
2023-06-17 21:44:54 -0300

    cor das bordas ao selecionar para digitar

--- vozTech/src/componentes/temas/cadastrotemas/CadastroTema.tsx
+++ vozTech/src/componentes/temas/cadastrotemas/CadastroTema.tsx
@@ -1,120 +1,139 @@
-import React, {useState, useEffect, ChangeEvent} from 'react'
-import { Container, Typography, TextField, Button } from "@material-ui/core"
+import React, { useState, useEffect, ChangeEvent } from 'react';
+import { Container, Typography, TextField, Button } from '@material-ui/core';
 import Tema from '../../../models/Tema';
 import { buscaId, post, put } from '../../../services/Service';
-import "./CadastroTema.css";
 import { useNavigate, useParams } from 'react-router-dom';
 import { TokenState } from '../../../store/tokens/tokensReducer';
 import { useSelector } from 'react-redux/es/exports';
 import { toast } from 'react-toastify';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    focusedInput: {
+        '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
+            borderColor: "#654236",
+        },
+        '& .MuiInputLabel-root.Mui-focused': {
+            color: "#654236",
+        },
+    },
+}));
 
 function CadastroTema() {
     let navigate = useNavigate();
-    const { id } = useParams<{id: string}>();
-    const token = useSelector<TokenState, TokenState["tokens"]>(
-        (state) => state.tokens
-    );
+    const { id } = useParams<{ id: string }>();
+    const token = useSelector<TokenState, TokenState['tokens']>((state) => state.tokens);
     const [tema, setTema] = useState<Tema>({
         id: 0,
-        descricao: ""
-    })
+        descricao: '',
+    });
+    const classes = useStyles();
 
     useEffect(() => {
-        if (token == "") {
-            toast.error("Você precisa estar logado", {
-                position: "top-center",
-                    autoClose: 2000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
+        if (token === '') {
+            toast.error('Você precisa estar logado', {
+                position: 'top-center',
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: 'colored',
             });
-            navigate("/login")
+            navigate('/login');
         }
-    }, [token])
+    }, [token, navigate]);
 
-    useEffect(() =>{
-        if(id !== undefined){
-            findById(id)
+    useEffect(() => {
+        if (id !== undefined) {
+            findById(id);
         }
-    }, [id])
+    }, [id]);
 
     async function findById(id: string) {
         buscaId(`/temas/${id}`, setTema, {
             headers: {
-                "Authorization": token
-            }
-        })
+                Authorization: token,
+            },
+        });
     }
 
     function updatedTema(e: ChangeEvent<HTMLInputElement>) {
         setTema({
             ...tema,
             [e.target.name]: e.target.value,
-        })
+        });
     }
 
     async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
-        e.preventDefault()
-        console.log("tema " + JSON.stringify(tema))
+        e.preventDefault();
+        console.log('tema ' + JSON.stringify(tema));
 
         if (id !== undefined) {
-            console.log(tema)
+            console.log(tema);
             put(`/temas`, tema, setTema, {
                 headers: {
-                    'Authorization': token
-                }
-            })
-            toast.success("Tema atualizado com sucesso", {
-                position: "top-center",
-                    autoClose: 2000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
+                    Authorization: token,
+                },
+            });
+            toast.success('Tema atualizado com sucesso', {
+                position: 'top-center',
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: 'colored',
             });
         } else {
             post(`/temas`, tema, setTema, {
                 headers: {
-                    'Authorization': token
-                }
-            })
-            toast.success("Tema cadastrado com sucesso", {
-                position: "top-center",
-                    autoClose: 2000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
+                    Authorization: token,
+                },
+            });
+            toast.success('Tema cadastrado com sucesso', {
+                position: 'top-center',
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: 'colored',
             });
         }
-        back()
-
+        back();
     }
 
     function back() {
-        navigate('/temas')
+        navigate('/temas');
     }
 
     return (
         <Container maxWidth="sm" className="topo">
             <form onSubmit={onSubmit}>
                 <Typography variant="h3" component="h1" align="center" className="corCadastreTema">
-                    {id !== undefined ? "Atualize " : "Cadastre "} seu Tema
-                    </Typography>
-                <TextField value={tema.descricao} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedTema(e)} id="descricao" label="descricao" variant="outlined" name="descricao" margin="normal" fullWidth />
-                <Button type="submit" variant="contained" className='botaoCadastreTema' disabled={tema.descricao.length < 4}>
+                    {id !== undefined ? 'Atualize ' : 'Cadastre '} seu Tema
+                </Typography>
+                <TextField
+                    value={tema.descricao}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => updatedTema(e)}
+                    id="descricao"
+                    label="descricao"
+                    variant="outlined"
+                    name="descricao"
+                    margin="normal"
+                    fullWidth
+                    className={classes.focusedInput}
+                />
+                <Button type="submit" variant="contained" className="botaoCadastreTema" disabled={tema.descricao.length < 4}>
                     Cadastrar Tema
                 </Button>
             </form>
         </Container>
-    )
+    );
 }
 
 export default CadastroTema;
--- vozTech/src/paginas/cadastroUsuario/CadastroUsuario.tsx
+++ vozTech/src/paginas/cadastroUsuario/CadastroUsuario.tsx
@@ -5,121 +5,216 @@ import { Link, useNavigate } from "react-router-dom";
 import { cadastroUsuario } from "../../services/Service";
 import User from "../../models/User";
 import { toast } from "react-toastify";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    input: {
+        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+            borderColor: "black", // Altere para a cor desejada
+        },
+        "& .MuiInputLabel-root.Mui-focused": {
+            color: "black", // Altere para a cor desejada
+        },
+    },
+}));
 
 function CadastroUsuario() {
+    const classes = useStyles();
     let navigate = useNavigate();
-    const [confirmarSenha, setConfirmarSenha] = useState<String>("")
-    const [user, setUser] = useState<User>(
-        {
-            id: 0,
-            nome: '',
-            usuario: '',
-            foto: '',
-            senha: '',
-        })
-    const [userResult, setUserResult] = useState<User>(
-        {
-            id: 0,
-            nome: '',
-            usuario: '',
-            foto: '',
-            senha: ''
-        })
+    const [confirmarSenha, setConfirmarSenha] = useState<String>("");
+    const [user, setUser] = useState<User>({
+        id: 0,
+        nome: "",
+        usuario: "",
+        foto: "",
+        senha: "",
+    });
+    const [userResult, setUserResult] = useState<User>({
+        id: 0,
+        nome: "",
+        usuario: "",
+        foto: "",
+        senha: "",
+    });
     useEffect(() => {
-        if (userResult.id != 0) {
-            navigate("/login")
+        if (userResult.id !== 0) {
+            navigate("/login");
         }
-    }, [userResult])
+    }, [userResult]);
+
     function confirmarSenhaHandle(e: ChangeEvent<HTMLInputElement>) {
-        setConfirmarSenha(e.target.value)
+        setConfirmarSenha(e.target.value);
     }
+
     function updatedModel(e: ChangeEvent<HTMLInputElement>) {
         setUser({
             ...user,
-            [e.target.name]: e.target.value
-        })
+            [e.target.name]: e.target.value,
+        });
     }
+
     async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
-        e.preventDefault()
+        e.preventDefault();
         if (user.senha.length >= 8) {
-            if (confirmarSenha == user.senha) {
+            if (confirmarSenha === user.senha) {
                 try {
                     await cadastroUsuario("/usuarios/cadastrar", user, setUserResult);
                     toast.success("Usuário cadastrado com sucesso", {
                         position: "top-center",
-                            autoClose: 2000,
-                            hideProgressBar: false,
-                            closeOnClick: true,
-                            pauseOnHover: true,
-                            draggable: true,
-                            progress: undefined,
-                            theme: "colored",
+                        autoClose: 2000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "colored",
                     });
                 } catch (error) {
                     toast.error("Falha ao cadastrar usuário, verifique os campos", {
                         position: "top-center",
-                            autoClose: 2000,
-                            hideProgressBar: false,
-                            closeOnClick: true,
-                            pauseOnHover: true,
-                            draggable: true,
-                            progress: undefined,
-                            theme: "colored",
+                        autoClose: 2000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "colored",
                     });
                 }
             } else {
                 toast.error("Os campos de Senha e Confirmar Senha estão diferentes", {
                     position: "top-center",
-                            autoClose: 2000,
-                            hideProgressBar: false,
-                            closeOnClick: true,
-                            pauseOnHover: true,
-                            draggable: true,
-                            progress: undefined,
-                            theme: "colored",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
                 });
                 setUser({ ...user, senha: "" });
-                setConfirmarSenha("")
+                setConfirmarSenha("");
             }
         } else {
-            toast.error("Os campos de Senha e Confirmar Senha precisam de, no mínimo, 8 caracteres", {
-                position: "top-center",
-                            autoClose: 2000,
-                            hideProgressBar: false,
-                            closeOnClick: true,
-                            pauseOnHover: true,
-                            draggable: true,
-                            progress: undefined,
-                            theme: "colored",
-            });
+            toast.error(
+                "Os campos de Senha e Confirmar Senha precisam de, no mínimo, 8 caracteres",
+                {
+                    position: "top-center",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                }
+            );
             setUser({ ...user, senha: "" });
-            setConfirmarSenha("")
+            setConfirmarSenha("");
         }
     }
+
     return (
-        <Grid container direction="row" justifyContent="center" alignItems="center" className="imagem2">
+        <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            className="imagem2"
+        >
             <Grid item xs={8} alignItems="center">
                 <Box paddingX={20} className="formulario2" justifyContent="center">
                     <form onSubmit={onSubmit}>
                         <Box className="ajuste">
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className="textos2">Cadastrar</Typography>
-                        <TextField className="espaco" value={user.nome} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="nome" label="Nome completo"  name="nome" margin="normal" fullWidth />
-                        <TextField className="espaco" value={user.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="usuario" label="Endereço de e-mail" variant="outlined" name="usuario" margin="normal" fullWidth />
-                        <TextField className="espaco" value={user.foto} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="foto" label="URL da Foto" variant="outlined" name="foto" margin="normal" fullWidth />
-                        <TextField className="espaco" value={user.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="senha" label="Senha" variant="outlined" name="senha" margin="normal" type="password" fullWidth />
-                        <TextField className="espaco" value={confirmarSenha} onChange={(e: ChangeEvent<HTMLInputElement>) => confirmarSenhaHandle(e)} id="confirmarSenha" label="Confirmar Senha" variant="outlined" name="confirmarSenha" margin="normal" type="password" fullWidth />
-                        <Box marginTop={2} textAlign="center">
-                            <Link to="/login" className="text-decoration-none">
-                                <Button variant="contained" color="secondary" className="btnCancelar">
-                                    Cancelar
-                                </Button>
-                            </Link>
-                            
-                            <Button type="submit" variant="contained" color="primary" className="btnCadastro">
+                            <Typography
+                                variant="h3"
+                                gutterBottom
+                                color="textPrimary"
+                                component="h3"
+                                align="center"
+                                className="textos2"
+                            >
                                 Cadastrar
-                            </Button>
+                            </Typography>
+                            <TextField
+                                className={`${classes.input} espaco`}
+                                value={user.nome}
+                                onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                                id="nome"
+                                label="Nome completo"
+                                variant="outlined"
+                                name="nome"
+                                margin="normal"
+                                fullWidth
+                            />
+                            <TextField
+                                className={`${classes.input} espaco`}
+                                value={user.usuario}
+                                onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                                id="usuario"
+                                label="Endereço de e-mail"
+                                variant="outlined"
+                                name="usuario"
+                                margin="normal"
+                                fullWidth
+                            />
+                            <TextField
+                                className={`${classes.input} espaco`}
+                                value={user.foto}
+                                onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                                id="foto"
+                                label="URL da Foto"
+                                variant="outlined"
+                                name="foto"
+                                margin="normal"
+                                fullWidth
+                            />
+                            <TextField
+                                className={`${classes.input} espaco`}
+                                value={user.senha}
+                                onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                                id="senha"
+                                label="Senha"
+                                variant="outlined"
+                                name="senha"
+                                margin="normal"
+                                type="password"
+                                fullWidth
+                            />
+                            <TextField
+                                className={`${classes.input} espaco`}
+                                value={confirmarSenha}
+                                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                                    confirmarSenhaHandle(e)
+                                }
+                                id="confirmarSenha"
+                                label="Confirmar Senha"
+                                variant="outlined"
+                                name="confirmarSenha"
+                                margin="normal"
+                                type="password"
+                                fullWidth
+                            />
+                            <Box marginTop={2} textAlign="center">
+                                <Link to="/login" className="text-decoration-none">
+                                    <Button
+                                        variant="contained"
+                                        color="secondary"
+                                        className="btnCancelar"
+                                    >
+                                        Cancelar
+                                    </Button>
+                                </Link>
 
-                        </Box>
+                                <Button
+                                    type="submit"
+                                    variant="contained"
+                                    color="primary"
+                                    className="btnCadastro"
+                                >
+                                    Cadastrar
+                                </Button>
+                            </Box>
                         </Box>
                     </form>
                 </Box>
@@ -128,4 +223,4 @@ function CadastroUsuario() {
     );
 }
 
-export default CadastroUsuario
+export default CadastroUsuario;
